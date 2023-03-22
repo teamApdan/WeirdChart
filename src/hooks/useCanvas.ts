99dfe0f3b5f96bcc171e42dd7f8ab6228f3aac8a
rkskekzzz <wkdlfflxh@naver.com>
2023-03-22 18:57:32 +0900

    ✨ roundRect 없는 브라우저 호환

--- src/hooks/useCanvas.ts
+++ src/hooks/useCanvas.ts
@@ -18,6 +18,21 @@ const useCanvas = (draw: (ctx: CanvasRenderingContext2D, frameCount: number) => 
     let frameCount = 0;
     let animationFrameId: number;
 
+    if (context.roundRect === undefined) {
+      context.roundRect = function (x, y, w, h, r: number) {
+        if (w < 2 * r) r = w / 2;
+        if (h < 2 * r) r = h / 2;
+        this.beginPath();
+        this.moveTo(x + r, y);
+        this.arcTo(x + w, y, x + w, y + h, r);
+        this.arcTo(x + w, y + h, x, y + h, r);
+        this.arcTo(x, y + h, x, y, r);
+        this.arcTo(x, y, x + w, y, r);
+        this.closePath();
+        return this;
+      };
+    }
+
     const render = () => {
       if (frameCount <= 1000) frameCount++;
       draw(context, frameCount);
