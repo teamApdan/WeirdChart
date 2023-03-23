358f023642bec9cf76e19642bab65f3cc25857c9
rkskekzzz <wkdlfflxh@naver.com>
2023-03-23 15:09:09 +0900

    ✨ roundRect 없는 브라우저 호환성 추가

--- src/hooks/useCanvas.ts
+++ src/hooks/useCanvas.ts
@@ -2,6 +2,38 @@ import { useRef, useEffect } from 'react';
 
 const useCanvas = (draw: (ctx: CanvasRenderingContext2D, frameCount: number) => void) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  if (CanvasRenderingContext2D.prototype.roundRect === undefined) {
+    CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r: number | number[]) {
+      let upperLeft = 0;
+      let upperRight = 0;
+      let lowerLeft = 0;
+      let lowerRight = 0;
+
+      if (typeof r === 'number') {
+        upperLeft = r;
+        upperRight = r;
+        lowerLeft = r;
+        lowerRight = r;
+      } else {
+        upperLeft = r[0];
+        upperRight = r[1];
+        lowerLeft = r[2];
+        lowerRight = r[3];
+      }
+      this.beginPath();
+      this.moveTo(x + upperLeft, y);
+      this.lineTo(x + w - upperRight, y);
+      this.quadraticCurveTo(x + w, y, x + w, y + upperRight);
+      this.lineTo(x + w, y + h - lowerRight);
+      this.quadraticCurveTo(x + w, y + h, x + w - lowerRight, y + h);
+      this.lineTo(x + lowerLeft, y + h);
+      this.quadraticCurveTo(x, y + h, x, y + h - lowerLeft);
+      this.lineTo(x, y + upperLeft);
+      this.quadraticCurveTo(x, y, x + upperLeft, y);
+      this.closePath();
+      this.fill();
+    };
+  }
 
   useEffect(() => {
     if (!canvasRef || !canvasRef.current) return;
@@ -14,24 +46,11 @@ const useCanvas = (draw: (ctx: CanvasRenderingContext2D, frameCount: number) => 
     canvas.height = canvas.offsetHeight * dpr;
 
     const context = canvas.getContext('2d');
+
     if (!context) return;
+
     let frameCount = 0;
     let animationFrameId: number;
-
-    if (context.roundRect === undefined) {
-      context.roundRect = function (x, y, w, h, r: number) {
-        if (w < 2 * r) r = w / 2;
-        if (h < 2 * r) r = h / 2;
-        this.beginPath();
-        this.moveTo(x + r, y);
-        this.arcTo(x + w, y, x + w, y + h, r);
-        this.arcTo(x + w, y + h, x, y + h, r);
-        this.arcTo(x, y + h, x, y, r);
-        this.arcTo(x, y, x + w, y, r);
-        this.closePath();
-        return this;
-      };
-    }
 
     const render = () => {
       if (frameCount <= 1000) frameCount++;
